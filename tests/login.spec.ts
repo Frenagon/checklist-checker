a88ee4ea19b523229b33b547cf5128318a2add1d
Federico Turban <fturbana@hotmail.com>
2026-05-07 13:05:31 -0600

    feat: create first test case script

---
+++ tests/login.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from "@playwright/test"
+import userData from "../fixtures/user-credentials.json"
+import { LoginPage } from "../pages/LoginPage"
+
+
+test ("Login Error", async ({page}) => {
+    const loginPage = new LoginPage(page);
+    
+    await loginPage.goToPage();
+
+    await loginPage.loginError(userData.user, userData.password);
+})
