5061e682a65d5255b02609849b6df515cdea353f
Francisco Aceves <frenagon@gmail.com>
2026-05-31 23:39:38 -0600

    feat: add registrations and attendance schemas

--- convex/schema.ts
+++ convex/schema.ts
@@ -16,4 +16,18 @@ export default defineSchema({
     title: v.string(),
     position: v.number(),
   }).index('by_eventId_and_position', ['eventId', 'position']),
+  registrations: defineTable({
+    userId: v.id('users'),
+    eventId: v.id('events'),
+    status: v.union(v.literal('registered'), v.literal('blocked')),
+  })
+    .index('by_userId_and_eventId', ['userId', 'eventId'])
+    .index('by_eventId_and_status', ['eventId', 'status']),
+  attendance: defineTable({
+    userId: v.id('users'),
+    activityId: v.id('activities'),
+    status: v.union(v.literal('attended'), v.literal('blocked')),
+  })
+    .index('by_userId_and_activityId', ['userId', 'activityId'])
+    .index('by_activityId_and_status', ['activityId', 'status']),
 });
