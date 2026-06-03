04fc814f2c0e961368847205e44037b1786f2ec6
LordTurban <fturbana@hotmail.com>
2026-06-03 13:53:33 -0600

    feat: adds github actions pipeline to run daily

--- tests/tests/example.spec.ts
+++ tests/tests/example.spec.ts
@@ -1,12 +1,5 @@
 import { expect, test } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/signin');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle('Convex + Next.js + ConvexAuth');
-});
-
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
